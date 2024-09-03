3962ceb7e347e9bd4d0fbfdc400d086236ac88ee
olubori <daveholuborhee@gmail.com>
2024-09-03 09:43:08 +0100

    create issue

--- backend/src/issues/entities/issue.entity.ts
+++ backend/src/issues/entities/issue.entity.ts
@@ -1,5 +1,6 @@
-import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
+@Entity()
 export class Issue {
     @PrimaryGeneratedColumn()
 	id: number;
@@ -10,9 +11,9 @@ export class Issue {
 	@Column({nullable: true})
 	description: string;
 
-    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+    @CreateDateColumn()
 	createdAt: Date;
 
-	@UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
+	@UpdateDateColumn()
 	updatedAt: Date;
 }
--- backend/src/issues/issues.service.ts
+++ backend/src/issues/issues.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateIssueDto } from './dto/create-issue.dto';
 import { UpdateIssueDto } from './dto/update-issue.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Issue } from './entities/issue.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class IssuesService {
+  constructor(
+    @InjectRepository(Issue)
+    private issueRepository: Repository<Issue>,
+  ) {}
   create(createIssueDto: CreateIssueDto) {
-    return 'This action adds a new issue';
+    const newIssue = this.issueRepository.create(createIssueDto);
+    return this.issueRepository.save(newIssue);
   }
 
   findAll() {
